e7aa27a9018e2c2810cebe92869a1da93da9c7f0
FrancoVeras <verastegui.franco@gmail.com>
2025-12-01 16:54:18 -0600

    projecto7

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Tripleten web_project_around
+
+## Proyecto 7: Alrededor de los EE.UU
+
+Siguiendo con la creación de proyectos, en esta ocación es la creacion de una red social para compartir momentos inolvidables.
+
+## Características del proyecto
+
+- HTML5 semántico
+- Flexbox
+- Position
+- Metodología BEM
+- CSS
+- FIGMA
+- GIT BASH
+- GITHUB
+- GITHUB PAGES
+- JAVASCRIPT
+
+## Funcionalidad
+
+La pagina es una galeria de arte, en la que se muestran los lugares de donde provienen nuestros colegas, reunidos en la ciudad Triple Ten iniciando con la bienvenida y descripción:
+
+    -Se muestra el perfil de usuario
+    -Sección de fotos
+    -Edición de perfil
+    -Boton like funcional
+
+## Mejoras
+
+En un futuro se agregaran mas features como agregar mas fotos y darle like.
+
+## Enlace a GitHub Pages
+
+     -https://github.com/FrancoVeras22/web_project_around
+
+## Contacto
+
+[FrancoVeras22] - [verastegui.franco@gmail.com]
---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// Elementos del DOM
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close-button");
+
+let nameInput = document.querySelector(".popup__name");
+let aboutInput = document.querySelector(".popup__about");
+
+let profileName = document.querySelector(".profile__name");
+let profileRole = document.querySelector(".profile__role");
+
+let profileForm = document.querySelector(".popup__form");
+
+// Abrir popup y rellenar inputs
+editButton.addEventListener("click", () => {
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileRole.textContent;
+  popup.classList.add("popup_show");
+});
+
+// Cerrar el popup
+closeButton.addEventListener("click", () => {
+  popup.classList.remove("popup_show");
+});
+// envio de cambio de datos
+profileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileRole.textContent = aboutInput.value;
+});
+// Like
+let hearts = document.querySelectorAll(".heart");
+
+hearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    heart.classList.toggle("liked");
+    heart.textContent = heart.classList.contains("liked") ? "♥" : "♡";
+  });
+});
